75ec727a8ecdb9021c9fdf5053e22f3516826ea4
carlavloureiro <carlavlc6@gmail.com>
2026-01-14 16:42:44 -0300

    feat: adiciona sorteio aleatório de categoria e palavra

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,37 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //Escolhe uma categoria aleatória
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories.length))];
+
+    //Escolhe uma palavra aleatória
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return{ word, category };
+  }
 
   //Começa o jogo
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    //Cria um array de letras, conforme a palavra escolhida
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
