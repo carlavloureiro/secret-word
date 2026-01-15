9807a2e217f492dfa802b64b3b02ee350e2aa54d
carlavloureiro <carlavlc6@gmail.com>
2026-01-15 10:28:08 -0300

    feat: consumindo states do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,11 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   const pickWordAndCategory = () => {
     //Escolhe uma categoria aleatória
     const categories = Object.keys(words);
@@ -68,7 +73,18 @@ function App() {
       <div className='app'>
         { /* a tela de inicio só será exibida quando o gamestage for start */ }
         {gameStage === "start" && <StartScreen startGame={startGame}/>}
-        {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+        {gameStage === "game" && (
+            <Game 
+              verifyLetter={verifyLetter} 
+              pickedWord={pickedWord} 
+              pickedCategory={pickedCategory} 
+              letters={letters}
+              guessedLetters={guessedLetters}
+              wrongLetters={wrongLetters}
+              guesses={guesses}
+              score={score}
+            />
+        )}
         {gameStage === "end" && <GameOver retry={retry}/>}
       </div>
     </>
